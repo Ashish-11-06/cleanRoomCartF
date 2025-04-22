5952b9a31e8e2030508d52cd6d2b534862465886
Ashish Bhosale <bhosaleashish311@gmail.com>
2025-04-22 14:36:41 +0530

    update BASE_URL for deployment

--- src/API/BaseURL.js
+++ src/API/BaseURL.js
@@ -1,2 +1,2 @@
 // src/API/BaseURL.js
-export const BASE_URL = "http://localhost:5001"; // Change this for deployment
+export const BASE_URL = "http://13.200.204.148/backend"; // Change this for deployment
